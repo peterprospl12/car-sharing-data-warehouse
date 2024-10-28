71db8bf58665ceb04d1443fa14ee664990d2ff8e
tomaszsankowski <sankowski.tomek@gmail.com>
2024-10-28 21:52:32 +0100

    Fixed 'always active' car_status problem

--- generator/generator.py
+++ generator/generator.py
@@ -12,10 +12,10 @@ from dateutil.relativedelta import relativedelta
 fake = Faker('pl_PL')
 fake.add_provider(VehicleProvider)
 
-N_USERS = 20  # Number of users
-N_CARS = 10  # Number of cars
-N_CAR_STATES = 50  # Number of car states per car (amount of car states >= N_CARS * N_PRICE_INCREASE)
-N_PRICE_INCREASES = 5  # Numer of pricelist changes
+N_USERS = 5  # Number of users
+N_CARS = 3  # Number of cars
+N_CAR_STATES = 3  # Number of car states per car (amount of car states >= N_CARS * N_PRICE_INCREASE)
+N_PRICE_INCREASES = 1  # Numer of pricelist changes
 N_RENTALS = 10  # Number of car rentals per vehicle (amount of car rentals >= N_CAR_STATES * N_USERS)
 
 # Possible car models with possible engine powers and luxury levels
@@ -138,103 +138,101 @@ def create_cars_car_states_and_rentals():
                     CAR_STATES_PER_PRICELIST = N_CAR_STATES // N_PRICE_INCREASES
 
                     for idx, pricelist_id in enumerate(pricelist_ids):
-                        for j in range(random.randint(0, CAR_STATES_PER_PRICELIST)):
+                        # Generate car state ID, and flag attributes
 
-                            # Generate car state ID, and flag attributes
+                        car_state_id = id_state
+                        id_state += 1
 
-                            car_state_id = id_state
-                            id_state += 1
+                        is_broken = fake.boolean(chance_of_getting_true=2)
+                        is_used = False if is_broken else fake.boolean(chance_of_getting_true=30)
 
-                            is_broken = fake.boolean(chance_of_getting_true=2)
-                            is_used = False if is_broken else fake.boolean(chance_of_getting_true=30)
+                        # is_active is true only if its last pricelist
+                        is_active = pricelist_id == pricelist_ids[-1]
 
-                            # is_active is true only if its last pricelist
-                            is_active = pricelist_id == pricelist_ids[-1]
+                        car_states_writer.writerow(
+                            [
+                                car_state_id,  # Car_State_ID
+                                is_broken,  # is_broken flag
+                                is_used,  # is_used flag
+                                utills.generate_random_city_coordinates()[1],  # Location
+                                is_active,  # is_used flag
+                                i + 1,  # Car_ID
+                                pricelist_id + 1  # Pricelist_ID
+                            ]
+                        )
 
-                            car_states_writer.writerow(
-                                [
-                                    car_state_id,  # Car_State_ID
-                                    is_broken,  # is_broken flag
-                                    is_used,  # is_used flag
-                                    utills.generate_random_city_coordinates()[1],  # Location
-                                    is_active,  # is_used flag
-                                    i + 1,  # Car_ID
-                                    pricelist_id + 1  # Pricelist_ID
-                                ]
+                        # For each car state generate from 0 to 2*N_RENTALS rentals
+
+                        rental_periods = []
+
+                        for k in range(random.randint(0, 2 * N_CAR_STATES)):
+
+                            # Generate rental ID
+
+                            rental_id = id_rental
+
+                            # Generate rental city and start and end locations
+
+                            city = random.choice(list(cities_coordinates.keys()))
+                            _, coordinates_start = utills.generate_random_city_coordinates(city=city)
+                            _, coordinates_end = utills.generate_random_city_coordinates(city=city)
+
+                            # Generate rental start and end dates
+
+                            start_date = fake.date_time_between(
+                                start_date=f'-{N_PRICE_INCREASES - idx}y',
+                                end_date=f'-{N_PRICE_INCREASES - idx - 1}y'
+                            )
+                            max_end_date = start_date + timedelta(hours=5, minutes=59, seconds=59)
+                            end_date_limit = datetime.now() - relativedelta(years=N_PRICE_INCREASES - idx - 1)
+                            end_date = fake.date_time_between(
+                                start_date=start_date,
+                                end_date=min(max_end_date, end_date_limit)
                             )
 
-                            # For each car state generate from 0 to 2*N_RENTALS rentals
+                            overlap_found = False
+                            for period in rental_periods:
+                                if start_date < period[1] and end_date > period[0]:
+                                    overlap_found = True
+                                    break
 
-                            rental_periods = []
+                            if not overlap_found:
+                                rental_periods.append((start_date, end_date))
+                            else:
+                                continue
 
-                            for k in range(random.randint(0, 2*N_CAR_STATES)):
+                            # Randomly select a user and check if they had a valid driving license at the time of rental
 
-                                # Generate rental ID
+                            user = users.sample(n=1).iloc[0]
+                            if user['License_receiving_date'] > start_date:
+                                continue
 
-                                rental_id = id_rental
+                            id_rental += 1
 
-                                # Generate rental city and start and end locations
+                            user_id = user['User_ID']
+                            driven_km = utills.calc_distance(coordinates_start, coordinates_end) + fake.random_int(
+                                min=1, max=30)
+                            layover_time = random.randint(0, 24)
 
-                                city = random.choice(list(cities_coordinates.keys()))
-                                _, coordinates_start = utills.generate_random_city_coordinates(city=city)
-                                _, coordinates_end = utills.generate_random_city_coordinates(city=city)
+                            # Calculate total cost
+                            total_cost = (pricelists[pricelist_id][1] +
+                                          pricelists[pricelist_id][2] * layover_time +
+                                          pricelists[pricelist_id][3] * driven_km)
 
-                                # Generate rental start and end dates
-
-                                start_date = fake.date_time_between(
-                                    start_date=f'-{N_PRICE_INCREASES - idx}y',
-                                    end_date=f'-{N_PRICE_INCREASES - idx - 1}y'
-                                )
-                                max_end_date = start_date + timedelta(hours=5, minutes=59, seconds=59)
-                                end_date_limit = datetime.now() - relativedelta(years=N_PRICE_INCREASES - idx - 1)
-                                end_date = fake.date_time_between(
-                                    start_date=start_date,
-                                    end_date=min(max_end_date, end_date_limit)
-                                )
-
-                                overlap_found = False
-                                for period in rental_periods:
-                                    if start_date < period[1] and end_date > period[0]:
-                                        overlap_found = True
-                                        break
-
-                                if not overlap_found:
-                                    rental_periods.append((start_date, end_date))
-                                else:
-                                    continue
-
-                                # Randomly select a user and check if they had a valid driving license at the time of rental
-                                
-                                user = users.sample(n=1).iloc[0]
-                                if user['License_receiving_date'] > start_date:
-                                    continue
-
-                                id_rental += 1
-
-                                user_id = user['User_ID']
-                                driven_km = utills.calc_distance(coordinates_start, coordinates_end) + fake.random_int(
-                                    min=1, max=30)
-                                layover_time = random.randint(0, 24)
-
-                                # Calculate total cost
-                                total_cost = (pricelists[pricelist_id][1] +
-                                              pricelists[pricelist_id][2] * layover_time +
-                                              pricelists[pricelist_id][3] * driven_km)
-
-                                rentals_writer.writerow(
-                                    [
-                                        rental_id,  # Rental_ID
-                                        start_date.strftime("%Y-%m-%d %H:%M:%S"),  # Rental_date_start
-                                        end_date.strftime("%Y-%m-%d %H:%M:%S"),  # Rental_date_end
-                                        coordinates_start,  # Start_location
-                                        coordinates_end,  # End_location
-                                        driven_km,  # Driven_km
-                                        layover_time,  # Layover_time
-                                        round(total_cost, 2),  # Total_cost
-                                        car_state_id,  # Car_state_ID
-                                        user_id  # User_ID
-                                    ]
-                                )
+                            rentals_writer.writerow(
+                                [
+                                    rental_id,  # Rental_ID
+                                    start_date.strftime("%Y-%m-%d %H:%M:%S"),  # Rental_date_start
+                                    end_date.strftime("%Y-%m-%d %H:%M:%S"),  # Rental_date_end
+                                    coordinates_start,  # Start_location
+                                    coordinates_end,  # End_location
+                                    driven_km,  # Driven_km
+                                    layover_time,  # Layover_time
+                                    round(total_cost, 2),  # Total_cost
+                                    car_state_id,  # Car_state_ID
+                                    user_id  # User_ID
+                                ]
+                            )
 
 
 def main():
